24a16c9ac774b05c869b403805fe738f4c5d3f73
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-10-20 08:44:06 +0300

    Initial import
    Classes: Chunk (descriptor), MetaHeader(metaclass), HeaderBuffer (base
    class for PolyHeader), PolyHeader (Polygon header)

---
+++ meta_header.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import BinaryIO
+import struct
+
+
+class Chunk:
+    def __init__(self, format, offset):
+        self.format: str = format  # e.g., "<i"
+        self.offset: int = offset
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        tup = struct.unpack_from(
+            self.fmt, instance._buffer + self.offset
+        )  # always a tupple
+        if len(tup) == 1:
+            return tup[0]
+        else:
+            return tup
+
+
+class MetaHeader(type):
+    def __init__(cls, clsname, bases, clsdict):
+        offset = 0
+        byte_code = ""
+        for format, field_name in cls._fields_:
+            if format[0] in ():
+                byte_code = format[0]
+                format = format[1:]
+            format = byte_code + format
+            setattr(cls, field_name, Chunk(format, offset))
+            offset += struct.calcsize(format)
+        setattr(cls, "size", offset)
+
+
+class HeaderBuffer:
+    def __init__(self, buffer):
+        self._buffer = memoryview(buffer)
+
+    @classmethod
+    def from_file(cls, f: BinaryIO):
+        return cls(f.read(cls.size))
+
+
+class PolyHeader(HeaderBuffer, metaclass=MetaHeader):
+    _fields_ = [
+        ("<i", "code"),
+        # code = Chunk("<i", 0)
+        ("dd", "min_x"),
+        # code = Chunk("dd", 4)
+        ("dd", "min_y"),
+        ("dd", "max_x"),
+        ("dd", "max_y"),
+        ("i", "npoly"),
+    ]
